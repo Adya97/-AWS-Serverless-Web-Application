c8470fd6ceb639b87e45faddf6299e6e6456d58c
Aditya Nisal <72440232+Adya97@users.noreply.github.com>
2024-01-04 18:57:58 +0100

    Second Commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+// Add your API invoke URL below, configured in Module 3. 
+
+module.exports = {
+    api: {
+        invokeUrl: 'https://blrhamo23l.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+    }
+}
